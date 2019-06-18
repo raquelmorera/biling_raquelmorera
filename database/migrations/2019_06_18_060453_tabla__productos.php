f66db1b4fefef477c258af31681c965c24d7474d
raquelmorera <48129437+raquelmorera@users.noreply.github.com>
2019-06-18 00:27:07 -0600

    3 tablas
    3

---
+++ database/migrations/2019_06_18_060453_tabla__productos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TablaProductos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_producto', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('cod_producto');
+            $table->string('nombre_producto', 100);
+            $table->string('detalle_producto', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_producto');
+    }
+}
